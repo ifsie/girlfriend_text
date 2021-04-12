cfa877191ac05b6ee3e88b3e510920aa20b6bf87
ifs <70446066+ifsie@users.noreply.github.com>
2021-04-12 08:12:48 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
 phone_number = "phone number"
-message = ["Good morning babe", "Good morning my love", "Good morning cutie"]
+message = ["kill yourself you fat peice of shit, honestly noone fucking loves you. your dog doesnt even let you pet him you're and utter disappointment to everyone around you", "fucking kill your self already we've been waiting for about a year now and it's making us suffer that you fucking exist", "kys"]
 scheduled_time = "08:00"
